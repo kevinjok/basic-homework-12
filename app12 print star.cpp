edfde7c990caf51d8eb4c257f966c7dd08c4bd0a
kevinjok <43134497+kevinjok@users.noreply.github.com>
2019-04-12 09:14:10 +0800

    Add files via upload

---
+++ app12 print star.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+int main(void)
+{
+    int n;
+    int w, x, y, z;
+    printf("輸入正方形的邊長 -> ");
+    scanf("%d", &n); 
+    
+
+    z = n;
+    for(y=1; y<=z; y++){
+       for(x=n; x<z; x++)
+          printf(" ");
+       for(w=1; w<=n; w++)
+          printf("*");
+       printf("\n");
+       n--;
+    }
+    
+    system("pause");
+    return 0;
+}
